49f7b47d92ecc253a685614bd6061a1ab8539470
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-05 21:05:40 +0000

    Updated

--- spinner2.js
+++ spinner2.js
@@ -1,14 +1,11 @@
-
-
-
 const spinnerPositions = ['\r|   ', '\r/   ', '\r-   ', '\r\\   '];
 
 let delay = 100;
 
-for (let positions = 0; positions < 3; positions++) {
+for (let cycle = 0; cycle < 2; cycle++) {
   for (let i = 0; i < spinnerPositions.length; i++) {
     setTimeout(() => {
-      process.stdout.write(spinnerPositions[position])
+      process.stdout.write(spinnerPositions[i])
     }, delay)
     delay += 200;
   }
